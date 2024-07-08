8170d9c9f757f65711030cdf3108b7126c1e4cf0
Dimitri <61880632+dvarmstrong@users.noreply.github.com>
2024-07-08 16:22:27 -0400

    node modules

---
+++ components/02-problem/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Form from "./Form";
+
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Form />
+        </div>
+    );
+
+}
+
+export default App;
